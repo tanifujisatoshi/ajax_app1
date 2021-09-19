f82109318a6e31569c42b36f845471eba912f68a
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-09-19 12:42:10 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+# postモデルに対してcreateメソッドを使うことで引数に指定した内容を保存することができる
+# 今回はcontentカラムの内容はビューから送られてきたparamsというハッシュのcontentというキーのバリューでありそれををpostテーブルに保存する
+   render json:{ post: post }
+# javascript側にキーがpostで(キー名は何でも良い)バリューがビューから送られてきた投稿の内容を保存したpostsテーブルの内容であるハッシュを送っている
+# そのバリューをjson形式のビューとして返すことでページの一部分しか読み込み直しをすることがない
+# redirect_toメソッドはページ全体が読み込み直されるためjson形式のビューを返すには不適切なのでrenderメソッド(ページが内容が変わることなく表示される)を使っている
   end
 end
--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,13 +1,28 @@
 function post()
    {const submit = document.getElementById("submit")
+//投稿するボタンの要素を取得して変数submitに代入している
      submit.addEventListener("click", (e) => {
      e.preventDefault()    
+//投稿するボタンをクリックすることでAjaxによるリクエストとブラウザからのリクエストが同時にサーバーサイドに送信される
+//今回は非同期通信による投稿のみを実装したいのでデフォルトでかかっているブラウザからのリクエストはキャンセルする必要がある
+//そのためclickイベントを意味するe(任意の文字列で良い)に対してprevent(=防ぐという意味)Defaultメソッドを使うことで投稿するボタンをクリックしてもAjaxによるリクエストしかサーバ側に送信されない
      const form = document.getElementById("form")
+//フォーム全体の要素を取得してそれを変数formに代入している
      const formData = new FormData(form)
+//フォームに入力された値を取得するためにFormDataオブジェクトをnewメソッドで生成して引数にはフォーム全体の要素を取得した変数を入れる
      const XHR = new XMLHttpRequest()
+//非同期通信を実装するためにXMLHttpRequest(javascriptを用いたHTTP通信)オブジェクトを生成してそれを変数XHRに代入している
      XHR.open("POST", "/posts", true)
+//そのHTTP通信に対してopenメソッドを使うことでリクエストの内容を指定することができる
+//今回のリクエストの内容としてはメモを投稿することなのでrails routes で POST /posts(.:format)  posts#createを参照しながら引数の内容を決める
+//第三引数には非同期通信を行うか行わないかを示すtrueかfalseを入れる
      XHR.responseType = "json"
+//リクエストを送信する際に、レスポンスで欲しいデータの型をあらかじめ指定しておく必要がある
+//レスポンスをどのようなデータの型にするかを指定するresponseTypeプロパティをXMLHttpRequest(javascriptを用いたHTTP通信)に対して使う必要がある
+//他の言語でも使うことができ動作が軽いjsonをresponseTypeプロパティで指定している
      XHR.send(formData)
+//sendメソッドを使うことでリクエストの内容をサーバーサイドに送信することができる
+//今回はフォームに入力した値をリクエストとしてサーバー側に送信したいのでsendメソッドの引数にformDataを指定している
     })
    }
 
